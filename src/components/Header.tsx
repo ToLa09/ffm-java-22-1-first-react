d4de50af42beee1b51707189beb4222ba515b23d
ToLa09 <landgraf-tobias@web.de>
2022-10-20 16:06:19 +0200

    feat: add axios fetch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./css/Header.css"
 
-export default function Header(){
+export default function Header(props: {
+    callback: (url: string) => void
+    nextPage: () => string[]
+    prevPage: () => string[]
+}){
+    let [nextPageNumber, setNextPageNumber] = useState<string>("2")
+
+    function nextPage() {
+        setNextPageNumber(props.nextPage()[1].split("=")[1])
+    }
+
+    function prevPage() {
+        props.prevPage()
+    }
+
     return <>
         <header>
             <h1>Rick and Morty App 9000</h1>
+            <div>
+                <button onClick={nextPage}>Seite {nextPageNumber}</button>
+                <button onClick={prevPage}>Rückwärts</button>
+            </div>
         </header>
     </>
 }
